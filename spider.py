0c012edf50995ef950f1017ced83c7086060e17d
Methew Guda <methewguda@gmail.com>
2016-05-28 20:04:53 -0300

    Added spider class

---
+++ spider.py
@@ -0,0 +1,31 @@
+from urllib.request import urlopen
+from link_finder import LinkFinder
+from general import *
+
+class Spider:
+
+    # Class variables (shared among all instances)
+
+    project_name = ''
+    base_url = ''
+    domain_name = ''
+    queue_file = ''
+    crawled_file = ''
+    queue = set()
+    crawled = set()
+
+    def __init__(self, project_name, base_url, domain_name):
+        Spider.project_name = project_name
+        Spider.base_url = base_url
+        Spider.domain_name = domain_name
+        Spider.queue_file = Spider.project_name + '/queue.txt'
+        Spider.crawled_file = Spider.project_name + '/crawled.txt'
+        self.boot()
+        self.crawl_page('First Spider', Spider.base_url)
+
+    @staticmethod
+    def boot(self):
+        create_project_dir(Spider.project_name)
+        create_data_files(Spider.project_name, Spider.base_url)
+        Spider.queue = file_to_set(Spider.queue_file)
+        Spider.crawled = file_to_set(Spider.crawled_file)
