f65784dcafc93dace617e6416762fd528da5cb9c
Methew Guda <methewguda@gmail.com>
2016-05-28 16:42:12 -0300

    Added function to store files data

--- general.py
+++ general.py
@@ -5,3 +5,12 @@ def create_project_dir(directory):
     if not os.path.exists(directory):
         print('Creating project ' + directory)
         os.mkdir(directory)
+
+# Create queue and crawled files (if not created)
+def create_data_files(project_name, base_url):
+    queue = project_name + 'queue.txt'
+    crawled = project_name + 'crawled.txt'
+    if not os.path.isfile(queue):
+        write_file(queue, base_url)
+    if not os.path.isfile(crawled):
+        write_file(crawled, '')
